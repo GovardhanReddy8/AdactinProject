6a2af3845ca244d13cf415a0efb0f07a50777d57
GovardhanReddy8 <reddygovardhan376@gmail.com>
2022-04-26 08:17:18 +0530

    task01

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,5 +1,22 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LoginPage {
+	WebDriver driver;
+
+	public void homePage() {
+		System.setProperty("webdriver.chrome.driver", "Drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://adactinhotelapp.com/");
+	}
+
+	public void doLogin(String path, String value) {
+		driver.findElement(By.xpath(path)).sendKeys(value);
+		driver.findElement(By.xpath(path)).click();
+
+	}
 
 }
--- src/test/java/pages/SearchHotelPage.java
+++ src/test/java/pages/SearchHotelPage.java
@@ -1,5 +1,15 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class SearchHotelPage {
+	WebDriver driver;
+
+	public void selectHotel(String select, String enter) {
+		driver.findElement(By.xpath(select)).sendKeys(enter);
+		driver.findElement(By.xpath(select)).click();
+
+	}
 
 }
